77e7c2ff591e7753a1e09af4a948369992ed656f
Jon Fawcett <jonfawcett10@gmail.com>
2020-06-01 16:03:43 -0400

    Initial Commit

---
+++ LoopFollow/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  LoopFollow
+//
+//  Created by Jon Fawcett on 6/1/20.
+//  Copyright © 2020 Jon Fawcett. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
