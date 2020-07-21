6f8f927642eec8df132ecac5cb7a0be9336bd97b
Jon Fawcett <jonfawcett10@gmail.com>
2020-07-21 15:26:42 -0400

    Change: rename to Pred.

--- LoopFollow/repository/UserDefaults.swift
+++ LoopFollow/repository/UserDefaults.swift
@@ -23,7 +23,7 @@ class UserDefaultsRepository {
         "SAGE",
         "CAGE",
         "Rec. Bolus",
-        "Pred. Min/Max"])
+        "Pred."])
     static let infoSort = UserDefaultsValue<[Int]>(key: "infoSort", default: [0,1,2,3,4,5,6,7,8,9])
     static let infoVisible = UserDefaultsValue<[Bool]>(key: "infoVisible", default: [true,true,true,true,true,true,true,true,true,true])
     
