668fa071591e3de4d101b43df58c4edb56e1f055
Ostap Korkuna <ostapco@gmail.com>
2021-09-26 17:56:19 -0700

    Midnight lines on a small chart

--- LoopFollow/Controllers/Timers.swift
+++ LoopFollow/Controllers/Timers.swift
@@ -87,8 +87,7 @@ extension MainViewController {
     }
     
     @objc func graphNowTimerDidEnd(_ timer:Timer) {
-        createNowAndDIALines()
-        createMidnightLines()
+        createVerticalLines()
     }
     
     // Runs a 60 second timer when an alarm is snoozed
