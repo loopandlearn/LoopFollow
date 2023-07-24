b6b8b0f26dc279e2c714f2cf50ca5e05505b0ae4
Jonas Björkert <jonas@bjorkert.se>
2023-07-24 20:50:01 +0200

    30min

--- LoopFollow/repository/UserDefaults.swift
+++ LoopFollow/repository/UserDefaults.swift
@@ -60,6 +60,7 @@ class UserDefaultsRepository {
     static let minBasalScale = UserDefaultsValue<Double>(key: "minBasalScale", default: 5.0)
     static let minBGScale = UserDefaultsValue<Float>(key: "minBGScale", default: 250.0)
     static let showDIALines = UserDefaultsValue<Bool>(key: "showDIAMarkers", default: true)
+    static let show30MinLine = UserDefaultsValue<Bool>(key: "show30MinLine", default: false)
     static let showMidnightLines = UserDefaultsValue<Bool>(key: "showMidnightMarkers", default: false)
     static let lowLine = UserDefaultsValue<Float>(key: "lowLine", default: 70.0)
     static let highLine = UserDefaultsValue<Float>(key: "highLine", default: 180.0)
