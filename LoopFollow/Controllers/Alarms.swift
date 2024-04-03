614f67c99bf253927605fc2f40fb989f0d6bf7ab
Jonas Björkert <jonas@bjorkert.se>
2024-04-03 20:05:30 +0200

    Adjustment of swedish text

--- LoopFollow/Controllers/Alarms.swift
+++ LoopFollow/Controllers/Alarms.swift
@@ -946,7 +946,7 @@ extension MainViewController {
                     stable: "och det är stabilt",
                     increase: "och det har ökat med",
                     decrease: "och det har minskat med",
-                    currentBGIs: "Aktuellt BG är"
+                    currentBGIs: "Blodockret är"
                 )
             case "en": fallthrough
             default:
