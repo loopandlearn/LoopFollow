ee0ae7d378bc33c92a95a8c06d7e7e092ad7505c
Jonas Björkert <jonas@bjorkert.se>
2024-05-11 21:12:53 +0200

    Display branch, build date, latest version, blacklisted version

---
+++ LoopFollow/helpers/GitHubService.swift
@@ -0,0 +1,45 @@
+//
+//  GitHubService.swift
+//  LoopFollow
+//
+//  Created by Jonas Björkert on 2024-05-11.
+//  Copyright © 2024 Jon Fawcett. All rights reserved.
+//
+
+import Foundation
+
+class GitHubService {
+    enum GitHubDataType {
+        case versionConfig
+        case blacklistedVersions
+        
+        var url: String {
+            switch self {
+            case .versionConfig:
+                return "https://raw.githubusercontent.com/loopandlearn/LoopFollow/main/Config.xcconfig"
+            case .blacklistedVersions:
+                return "https://raw.githubusercontent.com/loopandlearn/LoopFollow/main/blacklisted-versions.json"
+            }
+        }
+    }
+    
+    func fetchData(for dataType: GitHubDataType, completion: @escaping (Data?) -> Void) {
+        let urlString = dataType.url
+        guard let url = URL(string: urlString) else {
+            completion(nil)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data, error == nil else {
+                completion(nil)
+                return
+            }
+            if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 {
+                completion(data)
+            } else {
+                completion(nil)
+            }
+        }.resume()
+    }
+}
