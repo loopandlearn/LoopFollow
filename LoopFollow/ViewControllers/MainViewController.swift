7b7022ce05a5960c6cfcc2b50b18403b45cc6375
Jonas Björkert <jonas@bjorkert.se>
2024-05-31 21:52:30 +0200

    First version of iaps predictions

--- LoopFollow/ViewControllers/MainViewController.swift
+++ LoopFollow/ViewControllers/MainViewController.swift
@@ -112,6 +112,12 @@ class MainViewController: UIViewController, UITableViewDataSource, ChartViewDele
     var carbData: [carbGraphStruct] = []
     var overrideGraphData: [DataStructs.overrideStruct] = []
     var predictionData: [ShareGlucoseData] = []
+    var predictionDataOpenAPS: [String: [ShareGlucoseData]] = [
+        "COB": [],
+        "IOB": [],
+        "UAM": [],
+        "ZT": []
+    ]
     var bgCheckData: [ShareGlucoseData] = []
     var suspendGraphData: [DataStructs.timestampOnlyStruct] = []
     var resumeGraphData: [DataStructs.timestampOnlyStruct] = []
