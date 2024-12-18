1e1910b6df9541a551e387d2260943575d3d7f66
Jonas Björkert <jonas@bjorkert.se>
2024-12-18 20:41:43 +0100

    Do not show TRC for the Main branch

--- LoopFollow/Remote/Settings/RemoteSettingsView.swift
+++ LoopFollow/Remote/Settings/RemoteSettingsView.swift
@@ -43,8 +43,9 @@ struct RemoteSettingsView: View {
                     Picker("Remote Type", selection: $viewModel.remoteType) {
                         Text("None").tag(RemoteType.none)
                         Text("Nightscout").tag(RemoteType.nightscout)
-                        Text("Trio Remote Control").tag(RemoteType.trc)
-                    }
+                        if BuildDetails.default.branch?.lowercased() != "main" {
+                            Text("Trio Remote Control").tag(RemoteType.trc)
+                        }                    }
                     .pickerStyle(MenuPickerStyle())
 
                     Text("Nightscout is the only option available for the released version of Trio.")
