f1160992d53104ba6535033ffad233c05f941878
Jonas Björkert <jonas@bjorkert.se>
2025-03-01 19:29:09 +0100

    Correct estimated delay for RL

--- LoopFollow/BackgroundRefresh/BackgroundRefreshType.swift
+++ LoopFollow/BackgroundRefresh/BackgroundRefreshType.swift
@@ -34,6 +34,15 @@ enum BackgroundRefreshType: String, Codable, CaseIterable {
         }
     }
 
+    var estimatedDelayBasedOnHeartbeat: Bool {
+        switch self {
+        case .rileyLink:
+            return true
+        case .dexcom, .silentTune, .none:
+            return false
+        }
+    }
+
     /// Determines if a BLEDevice matches the specific device type
     func matches(_ device: BLEDevice) -> Bool {
         switch self {
