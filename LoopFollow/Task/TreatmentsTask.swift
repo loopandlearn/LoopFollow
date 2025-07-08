5c982e5981b02f7d31c09eb51c229b3bcdbe2f30
Jonas Björkert <jonas@bjorkert.se>
2025-07-08 18:58:29 +0200

    Debounce alarm checks and improve scheduling logic

--- LoopFollow/Task/TreatmentsTask.swift
+++ LoopFollow/Task/TreatmentsTask.swift
@@ -23,6 +23,6 @@ extension MainViewController {
         WebLoadNSTreatments()
 
         TaskScheduler.shared.rescheduleTask(id: .treatments, to: Date().addingTimeInterval(2 * 60))
-        TaskScheduler.shared.rescheduleTask(id: .alarmCheck, to: Date())
+        TaskScheduler.shared.rescheduleTask(id: .alarmCheck, to: Date().addingTimeInterval(3))
     }
 }
