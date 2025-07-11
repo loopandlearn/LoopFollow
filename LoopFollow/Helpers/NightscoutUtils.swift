212ea49142be6e2464811aa100adda2cc5438d98
Jonas Björkert <jonas@bjorkert.se>
2025-07-11 13:36:42 +0200

    sha1 is no longer needed

--- LoopFollow/Helpers/NightscoutUtils.swift
+++ LoopFollow/Helpers/NightscoutUtils.swift
@@ -5,18 +5,6 @@
 import CommonCrypto
 import Foundation
 
-extension String {
-    var sha1: String {
-        let data = Data(utf8)
-        var digest = [UInt8](repeating: 0, count: Int(CC_SHA1_DIGEST_LENGTH))
-        data.withUnsafeBytes {
-            _ = CC_SHA1($0.baseAddress, CC_LONG(data.count), &digest)
-        }
-        let hexBytes = digest.map { String(format: "%02hhx", $0) }
-        return hexBytes.joined()
-    }
-}
-
 class NightscoutUtils {
     enum NightscoutError: Error, LocalizedError {
         case emptyAddress
