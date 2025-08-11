8d4973f8695729e8eb43561d5fb56b3df5924335
codebymini <daniel@codebymini.se>
2025-08-11 14:52:33 +0200

    Remove stray comment from storage

--- LoopFollow/Storage/Storage+Migrate.swift
+++ LoopFollow/Storage/Storage+Migrate.swift
@@ -293,9 +293,6 @@ extension Storage {
                 $0.ignoreZeroBG = $1
             }
 
-            // 10. Volume button silence (new feature, defaults to true)
-            // No migration needed as it defaults to true in the struct
-
             // finally persist the whole struct
             Storage.shared.alarmConfiguration.value = cfg
         }
