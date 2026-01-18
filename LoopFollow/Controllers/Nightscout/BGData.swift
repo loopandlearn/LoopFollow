68fabc6e20b42d739d2ea8f56b0a58568c2c3e94
hmatax-tom <tom@hma.tax>
2026-01-18 18:12:54 +1300

    Update BGData.swift
    Fix dataString unreliability issue

--- LoopFollow/Controllers/Nightscout/BGData.swift
+++ LoopFollow/Controllers/Nightscout/BGData.swift
@@ -51,10 +51,9 @@ extension MainViewController {
         }
 
         var parameters: [String: String] = [:]
-        let utcISODateFormatter = ISO8601DateFormatter()
         let date = Calendar.current.date(byAdding: .day, value: -1 * Storage.shared.downloadDays.value, to: Date())!
         parameters["count"] = "\(Storage.shared.downloadDays.value * 2 * 24 * 60 / 5)"
-        parameters["find[dateString][$gte]"] = utcISODateFormatter.string(from: date)
+        parameters["find[date][$gte]"] = "\(Int(date.timeIntervalSince1970 * 1000))"
 
         // Exclude 'cal' entries
         parameters["find[type][$ne]"] = "cal"
