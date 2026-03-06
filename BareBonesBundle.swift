d246a08cd50d7f19e5dfb7d2505cd22b9ee6f8ce
MtlPhil <76601115+MtlPhil@users.noreply.github.com>
2026-03-06 14:03:29 -0500

    Create BareBonesBundle.swift

---
+++ BareBonesBundle.swift
@@ -0,0 +1,16 @@
+// BareBonesBundle.swift
+// Philippe Achkar
+// 2026-03-06
+//
+// Extension target only.
+// This is the @main entry point for the Widget Extension.
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct BareBonesBundle: WidgetBundle {
+var body: some Widget {
+BareBonesLiveActivityWidget()
+}
+}
