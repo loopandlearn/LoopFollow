f54977226ce209959a44d14dae68fc4941467227
Phil A <76601115+MtlPhil@users.noreply.github.com>
2026-03-23 15:22:37 -0400

    Update LoopFollowLABundle.swift

--- LoopFollowLAExtension/LoopFollowLABundle.swift
+++ LoopFollowLAExtension/LoopFollowLABundle.swift
@@ -6,11 +6,23 @@ import WidgetKit
 
 @main
 struct LoopFollowLABundle: WidgetBundle {
+    @WidgetBundleBuilder
+    var body: some Widget {
+        LegacyLiveActivityBundle()
+        ModernLiveActivityBundle()
+    }
+}
+
+@available(iOSApplicationExtension, introduced: 16.1, obsoleted: 18.0)
+struct LegacyLiveActivityBundle: WidgetBundle {
     var body: some Widget {
         LoopFollowLiveActivityWidget()
+    }
+}
 
-        if #available(iOS 18.0, *) {
-            LoopFollowLiveActivityWidgetWithCarPlay()
-        }
+@available(iOSApplicationExtension 18.0, *)
+struct ModernLiveActivityBundle: WidgetBundle {
+    var body: some Widget {
+        LoopFollowLiveActivityWidgetWithCarPlay()
     }
 }
